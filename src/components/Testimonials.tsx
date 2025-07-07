926965db2bbdf38ef85ceb63d96e0a8845fc2b1f
jjamesliu <liujameszhi@gmail.com>
2025-07-07 16:06:22 -0700

    all fixed

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -2,10 +2,10 @@ export default function Testimonials() {
     return (
         <div className="flex flex-col items-center justify-center mt-28 ">
             <div className='mb-16'>
-                <h1 className="text-5xl text-center font-bold">See what the <span className="text-[rgb(255,163,0)] underline">Inner Circle</span> is saying</h1>
+                <h1 className="text-4xl text-center font-bold">See what the <span className="text-[rgb(255,163,0)] underline">Inner Circle</span> is saying</h1>
             </div>
 
-            <div className="grid grid-cols-2 gap-20">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
 
             <div className="bg-gradient-to-r from-[rgb(255,203,47)] to-[rgb(255,149,0)] p-4 rounded-lg shadow-md">
                 <video 
