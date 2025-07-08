7b9a817f54a34bd524c85fa4f1b9e8e95dd3e81e
jjamesliu <liujameszhi@gmail.com>
2025-07-08 14:16:24 -0700

    added logo on top left

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 export default function Header() {
     return (
+      <>
         <a href="https://calendly.com/amazonmike2006/30min?month=2025-07" target="_blank" className='cursor-default'>
       <div className="bg-red-600 text-center py-2 px-4 animate-pulse">
         <p className="text-sm font-bold">
@@ -7,5 +8,10 @@ export default function Header() {
         </p>
       </div>
       </a>
+
+      <div>
+        <img src="amzwinners.jpg" className='w-[100px] mt-[1.3rem] ml-[1.3rem]'/>
+      </div>
+      </>
     )
 }
