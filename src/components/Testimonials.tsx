15d8fdbf96628df64f4ba1ff4198844dfa5aef63
jjamesliu <liujameszhi@gmail.com>
2025-07-09 22:41:08 -0700

    added youtube testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -5,22 +5,46 @@ export default function Testimonials() {
                 <h1 className="text-4xl text-center font-bold">See what the <span className="text-[rgb(255,163,0)] underline">Inner Circle</span> is saying</h1>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-[4rem]">
 
             <div className="bg-gradient-to-r from-[rgb(255,203,47)] to-[rgb(255,149,0)] p-4 rounded-lg shadow-md">
-                <video 
-                src="./image/first_testimonial.mov" 
-                controls 
-                className="max-h-[45rem] rounded-md"
-                />
+                    <iframe 
+                      className="w-full h-[35rem]"
+                      src="https://www.youtube.com/embed/ayjZIIoXTfM" 
+                      title="How to Start and Scale your Amazon Business"  
+                      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                      allowFullScreen
+                    />
             </div>
 
             <div className="bg-gradient-to-l from-[rgb(255,203,47)] to-[rgb(255,149,0)] p-4 rounded-lg shadow-md">
-                <video 
-                src="./image/second_testimonial.mov" 
-                controls 
-                className=" max-h-[45rem] rounded-md"
-                />
+                    <iframe 
+                      className="w-full h-[35rem]"
+                      src="https://www.youtube.com/embed/NuCFQE9M7lw" 
+                      title="How to Start and Scale your Amazon Business"  
+                      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                      allowFullScreen
+                    />
+            </div>
+
+            <div className="bg-gradient-to-l from-[rgb(255,203,47)] to-[rgb(255,149,0)] p-4 rounded-lg shadow-md">
+                    <iframe 
+                      className="w-full h-[35rem]"
+                      src="https://www.youtube.com/embed/EF3YgelYJSk" 
+                      title="How to Start and Scale your Amazon Business"  
+                      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                      allowFullScreen
+                    />
+            </div>
+
+            <div className="bg-gradient-to-l from-[rgb(255,203,47)] to-[rgb(255,149,0)] p-4 rounded-lg shadow-md">
+                    <iframe 
+                      className="w-full h-[35rem]"
+                      src="https://www.youtube.com/embed/wrmi_Fi4ENw" 
+                      title="How to Start and Scale your Amazon Business"  
+                      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                      allowFullScreen
+                    />
             </div>
 
             </div>
