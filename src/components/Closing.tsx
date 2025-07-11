9d94ce908e5b7aaf0640b2154f5331a8620d7dbf
jjamesliu <liujameszhi@gmail.com>
2025-07-11 13:42:47 -0700

    almost final

--- src/components/Closing.tsx
+++ src/components/Closing.tsx
@@ -3,7 +3,7 @@ import CTAButton from './CTAButton'
 export default function Closing() {
     return (
         <div className='mt-[3rem]'>
-            <h1 className="font-bold text-center mt-[7rem] text-3xl max-w-[40rem] mx-auto">Start <span className='text-foreground'>building</span> your own <span className='text-[#ffa300] underline'>profitable Amazon Business</span> Today!</h1>
+            <h1 className="font-bold text-center mt-[7cdrem] text-3xl max-w-[40rem] mx-auto">Start <span className='text-foreground'>building</span> your own <span className='text-[#ffa300] underline'>profitable Amazon Business</span> Today!</h1>
             <CTAButton />
         </div>
     )
